9e31d4c584921759a863f9649652b30cbb24dffd
truth809 <truth809@naver.com>
2021-02-06 15:10:16 +0900

    commit

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -3,7 +3,7 @@ import Dropzone from 'react-dropzone'
 import { PlusOutlined } from '@ant-design/icons';
 import axios from 'axios';
 
-function FileUpload() {
+function FileUpload(props) {
     
     const [Images, setImages] = useState([])
 
@@ -19,6 +19,7 @@ function FileUpload() {
             .then(response => {
                 if (response.data.success) {
                     setImages([...Images, response.data.filePath])
+                    props.refreshFunction([...Images, response.data.filePath])
                 } else {
                     alert('파일을 저장하는데 실패했습니다.')
                 }
@@ -28,11 +29,10 @@ function FileUpload() {
 
     const deleteHandler = (image) => {
         const currentIndex = Images.indexOf(image)
-
         let newImages = [...Images]
         newImages.splice(currentIndex, 1)
-
         setImages(newImages)
+        props.refreshFunction(newImages)
     }
 
 
--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
-import { Typography, Button, Form, Input } from 'antd';
+import { Button, Form, Input } from 'antd';
 import FileUpload from '../../utils/FileUpload.js';
+import Axios from 'axios';
 
 const { TextArea } = Input;
 
@@ -14,7 +15,7 @@ const Continents = [
     { key: 7, value: "Antarctica" }
 ]
 
-function UploadProductPage() {
+function UploadProductPage(props) {
     
     const [Title, setTitle] = useState("")
     const [Description, setDescription] = useState("")
@@ -37,19 +38,48 @@ function UploadProductPage() {
     const continentChangeHandler = (event) => {
         setContinent(event.currentTarget.value)
     }
+
+    const updateImages = (newImages) => {
+        setImages(newImages)
+    }
+
+    const submitHandler = (event) => {
+        console.log('click');
+        event.preventDefault();
+        if (!Title || !Description || !Price || !Continent || !Images.length === 0) {
+            return alert("모든 값을 넣어주셔야 합니다.")
+        }
+        // 서버에 채운 값들을 request로 보낸다.
+
+        const body = {
+            // 로그인 된 사람의 ID
+            writer: props.user.userData._id,
+            title: Title,
+            description: Description,
+            price: Price,
+            images: Images,
+            continents: Continent
+        }
+        
+        Axios.post("/api/product", body)
+        .then(response => {
+            if(response.data.success) {
+                alert("상품 업로드에 성공했습니다.")
+                props.history.push('/')
+            } else {
+                alert("상품 업로드에 실패했습니다.")
+            }
+        })
+    }
     
     return (
         <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
             <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
                 <h2> 여행 상품 업로드</h2>
             </div>
-
-            <Form>
-                
+            <Form onSubmit={submitHandler}>
                 {/* DropZone */}
-
-                <FileUpload />
-                
+                <FileUpload refreshFunction={updateImages} />
                 <br />
                 <br />
                 <label>이름</label>
@@ -64,19 +94,16 @@ function UploadProductPage() {
                 <Input type="number" onChange={priceChangeHandler} value={Price} />
                 <br />
                 <br />
-                <select onChange={continentChangeHandler}>
-
+                <select onChange={continentChangeHandler} >
                     {Continents.map(item => (
                         <option key={item.key} value={Continent}> {item.value}</option>
                     ))}
-                    
                 </select>
                 <br />
                 <br />
-                <Button>
+                <Button type="submit">
                     확인
                 </Button>
-
             </Form>
         </div>
     )
