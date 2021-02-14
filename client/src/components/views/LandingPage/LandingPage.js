fc605c9f3c29dca8264c1058af4dc6396e7d688f
truth809 <truth809@naver.com>
2021-02-14 22:06:21 +0900

    bogi2

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -11,28 +11,45 @@ function LandingPage() {
     const [Products, setProducts] = useState([])
     const [Skip, setSkip] = useState(0)
     const [Limit, setLimit] = useState(8)
+    const [PostSize, setPostSize] = useState(0)
 
     useEffect(() => {
 
         let body = {
             skip: Skip,
             limit: Limit
-
         }
-       
-        axios.post('/api/product/products', body)
-            .then(response => {
-                if(response.data.success) {
 
-                    setProducts(response.data.productInfo)
-                } else {
-                    alert("상품들을 가져오는데 실패 했습니다.")
-                }
-            })
+        getProducts(body)
         
     }, [])
 
+    const getProducts = (body) => {
+        axios.post('/api/product/products', body)
+        .then(response => {
+            if(response.data.success) {
+                if(body.loadMore) {
+                    setProducts([...Products, ...response.data.productInfo])
+                } else {
+                    setProducts(response.data.productInfo)
+                }
+                setPostSize(response.data.postSize)
+            } else {
+                alert("상품들을 가져오는데 실패 했습니다.")
+            }
+        })
+    }
+
     const loadMoreHandler = () => {
+        let skip = Skip + Limit
+        let body = {
+            skip: Skip,
+            limit: Limit,
+            loadMore: true
+        }
+        
+        getProducts(body)
+        setSkip(skip)
 
     }
 
@@ -70,11 +87,13 @@ function LandingPage() {
             </Row>
             
             
-            
+            {PostSize >= Limit &&
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <button onClick={loadMoreHandler}>더보기</button>
+                </div>
+            }
 
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <button onClick={loadMoreHandler}>더보기</button>
-            </div>
+            
 
         </div>
     )
